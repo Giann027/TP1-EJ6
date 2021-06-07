ec0c6200ed98324b60d4fa5c6e9216af6f42a612
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 18:48:46 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TP1_EJ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buenass");
+            Console.Title = "Conversión de Grados";
+
+            double Reaumur, Fahrenheit, Celcius;
+
+            Console.Write("Ingrese la cantidad de Grados Celcius: ");
+            Celcius = Convert.ToSingle(Console.ReadLine());
+
+            Reaumur = 0.8 * Celcius;
+            Fahrenheit = (1.8 * Celcius) + 32;
+
+            Console.WriteLine($"{Celcius} grados celcius equivalen a {Reaumur} grados Reaumur y {Fahrenheit} grados Fahrenheit" );
+
+        
+        }
+    }
+}
